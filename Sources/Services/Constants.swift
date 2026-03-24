7287125c891977ab5b87d41270e95b25a53c344d
mustafa <mustafa52@icloud.com>
2026-03-24 15:27:07 -0700

    fix: update Google Apps Script URL

--- Sources/Services/Constants.swift
+++ Sources/Services/Constants.swift
@@ -3,5 +3,5 @@ import Foundation
 struct Constants {
     static let geminiApiKey = "AIzaSyB5I2zlbX1L3X5wFwxCCIuJuExh-_AvOJ0"
     static let cloudflareApiUrl = "https://fmb-kitchen-survey-cloudflare.pages.dev/api"
-    static let gasScriptUrl = "https://script.google.com/macros/s/AKfycbxlNPxdaHcXXZ0OnL5AB7J_pV6kkGbiQGcpt456jsRyVJhsyPpi7lfzh_KZy7RWRpjz/exec"
+    static let gasScriptUrl = "https://script.google.com/macros/s/AKfycbzBO-SBLz7nlFsgad1iIhuljCNGANdLhwNlb957A--l2fGgZwibhaLMkY3je5STsA_Uvw/exec"
 }
